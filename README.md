1905e55d3b6aa2eb3f3442a58f437c93409c5f95
Amet13 <admin@amet13.name>
2017-11-25 15:24:21 +0300

    Added tg bot

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Withdraw your money from Transferwise account with a favorable exchange rate.
 ## Usage
 
 Clone repo:
-```bash
-git clone https://github.com/Amet13/twertb
+```
+$ git clone https://github.com/Amet13/twertb
 ```
 
 Get help:
-```bash
-python3 twertb/ -h
+```
+$ python3 twertb/ -h
 usage: [-h] -s SOURCE -t TARGET -g GETDATA
 
 optional arguments:
@@ -25,19 +25,42 @@ optional arguments:
 ```
 
 Get last currency database to json file and check currency for `EUR/USD`:
-```bash
-python3 twertb -s EUR -t USD -g yes
+```
+$ python3 twertb -s EUR -t USD -g yes
 1EUR = 1.193USD at 2017-11-25T11:24:53+0000
 ```
 
 Check currency for `GBP/RUB` without updating database:
-```bash
-python3 twertb -s GBP -t RUB -g no
+```
+$ python3 twertb -s GBP -t RUB -g no
 1GBP = 77.88126RUB at 2017-11-25T11:24:57+0000
+```
+
+## Use Telegram
+
+* go to [@BotFather](https://t.me/BotFather) and create `/newbot`, for example `TWERTB_bot`
+* then you have token like `111111111:ABCDE...`
+* after go to [@MyTelegramID_bot](https://t.me/MyTelegramID_bot) and `/start` it
+* then you have your telegram ID like `123456789`
+
+Now you can run script with your token and ID:
+```
+$ python3 twertb -s EUR -t USD -g no --token 111111111:ABCDE --id 123456789
+1EUR = 1.193USD at 2017-11-25T12:04:46+0000
+Message sent to Telegram
+```
+
+![](https://raw.githubusercontent.com/Amet13/twertb/master/screenshot.png)
+
+## Autoupdate databases
+
+```
+$ crontab -e
+*/5 * * * * cd /path/to/twertb/ ; /usr/bin/python3 __main__.py -s EUR -t USD -g yes --token 111111111:ABCDE --id 123456789 &> /dev/null
 ```
 
 ## TODO
 
-* Telegram bot
+* Telegram bot (client/server)
 * TravisCI cron autoupdate database
 * Free hosting for it
--- twertb.py
+++ twertb.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from urllib.request import urlopen, Request
+from urllib.parse import urlencode
 import re, json, argparse, os.path
 
 tw_domain = 'https://transferwise.com'
 tw_url = tw_domain + '/tools/exchange-rate-alerts/'
 tw_header = 'Mozilla/5.0'
 tw_api_url = 'https://api.transferwise.com/v1/rates'
-output_json_file = 'currencies.json'
+output_json_file = 'misc/currencies.json'
 
 def checkFile():
     return os.path.exists(output_json_file)
@@ -18,17 +19,29 @@ def parseArguments():
     parser.add_argument('-s', '--source', dest='SOURCE', required=True, help='source currency')
     parser.add_argument('-t', '--target', dest='TARGET', required=True, help='target currency')
     parser.add_argument('-g', '--get-data', dest='GETDATA', required=True, help='set \'yes\' if need to update database?')
+    parser.add_argument('--token', dest='TGTOKEN', default='', help='set telegram token')
+    parser.add_argument('--id', dest='TGID', default='', help='set telegram  id')
     namespace = parser.parse_args()
-    global source, target, get_data
+
+    global source, target, get_data, tg_token, tg_id
     source = namespace.SOURCE
     target = namespace.TARGET
     update_currency = namespace.GETDATA
+
     if source == target:
         print('You can\'t convert {0} to same currency {1}'.format(source, target))
     if update_currency == 'yes':
         get_data = True
     else:
         get_data = False
+
+    #print(namespace.TGTOKENID[0])
+    try:
+        tg_token = namespace.TGTOKEN
+        tg_id = namespace.TGID
+    except(IndexError):
+        tg_token = ''
+        tg_id = ''
 
 def getJsLink():
     url = Request(tw_url)
@@ -56,11 +69,16 @@ def getCurrency():
     currency_data = json.loads(open(output_json_file).read())
     for each in currency_data:
         if each['source'] == source and each['target'] == target:
-            print('1{0} = {1}{2} at {3}'.format(source, each['rate'], target, each['time']))
+            global exchange_result
+            exchange_result = '1{0} = {1}{2} at {3}'.format(source, each['rate'], target, each['time'])
+            print(exchange_result)
 
-#def sendToTelegram():
-#    tgurl = 'https://api.telegram.org/bot'
-#    tgfull = '{0}{1}/sendMessage'.format(tgurl, tgtoken)
+def sendToTelegram():
+    tg_url = 'https://api.telegram.org/bot'
+    tg_full = '{0}{1}/sendMessage'.format(tg_url, tg_token)
+    tg_params = urlencode({'chat_id': tg_id, 'text': exchange_result}).encode('utf-8')
+    urlopen(tg_full, tg_params)
+    print('Message sent to Telegram')
 
 # Check if currency.json exists
 if checkFile() is False:
@@ -75,3 +93,6 @@ if get_data is True:
 
 # Get currency
 getCurrency()
+
+if tg_token != '' and tg_id != '':
+    sendToTelegram()
